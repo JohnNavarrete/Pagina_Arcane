def3d722700ca03aa8576ef9c6842ef8a157b91f
JohnNavarrete <johnperez2000@hotmail.com>
2021-12-10 21:37:13 -0600

    Seccion de Capitulos agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 import { PersonajesComponent } from './personajes/personajes.component';
+import { CapitulosComponent } from './capitulos/capitulos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ContentComponent,
-    PersonajesComponent
+    PersonajesComponent,
+    CapitulosComponent
   ],
   imports: [
     BrowserModule

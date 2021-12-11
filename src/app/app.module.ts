450009b3721d21fb124493f209aaa4111a08b5b2
JohnNavarrete <johnperez2000@hotmail.com>
2021-12-10 23:40:41 -0600

    Seccion de Contenido Extra agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 import { PersonajesComponent } from './personajes/personajes.component';
 import { CapitulosComponent } from './capitulos/capitulos.component';
+import { ExtraComponent } from './extra/extra.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { CapitulosComponent } from './capitulos/capitulos.component';
     HeaderComponent,
     ContentComponent,
     PersonajesComponent,
-    CapitulosComponent
+    CapitulosComponent,
+    ExtraComponent
   ],
   imports: [
     BrowserModule

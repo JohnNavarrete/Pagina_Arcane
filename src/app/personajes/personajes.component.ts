b8c8b800093043dbfb39ea50ca4a103d9f6e6b6a
JohnNavarrete <johnperez2000@hotmail.com>
2021-12-10 20:27:21 -0600

    Initial commit

---
+++ src/app/personajes/personajes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'personajes',
+  templateUrl: './personajes.component.html',
+  styleUrls: ['./personajes.component.css']
+})
+export class PersonajesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
